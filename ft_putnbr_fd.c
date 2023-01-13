cce56cc6e1e0708be33751f21a8264861d598fd7
alexcouture <alexcouture5@hotmail.com>
2023-01-12 21:20:20 -0500

    putnbr

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -2,12 +2,19 @@
 
 void ft_putnbr_fd(int n, int fd)
 {
-    if (n >= '0' && n <= '9')
-        ft_putchar_fd(n, fd);
-}
-
-int main()
-{
-    int n = 6;
-    ft_putnbr_fd(n, STDOUT_FILENO);
+    if (n == -2147483648)
+        write(fd, "-2147483648", 11);
+    if ()
+    if (n < 0)
+    {
+        ft_putchar_fd('-', fd);
+        n = -n;
+    }
+    if (n > 9)
+    {
+        ft_putnbr_fd(n / 10, fd);
+        n = n % 10;
+    }
+    if (n < 10)
+        ft_putchar_fd(n + 48, fd);
 }
