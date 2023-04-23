0ce0ba4d9bc8d86f7769170bc570c026c95056fa
Alexandre Couture <acouture@c1r7p12.42quebec.com>
2023-04-23 13:54:11 -0400

    commented the functions

--- ft_error.c
+++ ft_error.c
@@ -6,12 +6,16 @@
 /*   By: acouture <acouture@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/10 07:49:12 by acouture          #+#    #+#             */
-/*   Updated: 2023/04/02 09:14:03 by acouture         ###   ########.fr       */
+/*   Updated: 2023/04/23 13:02:40 by acouture         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/**
+ * @brief Displays on the STDERR a given message and exits the program
+ * @param s The message to be printed
+*/
 void	ft_error(char *s)
 {
 	write(2, s, ft_strlen(s));
